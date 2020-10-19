bc21fd41364630a4fe193771bd914037f449141f
Xiran XU <xx2368@columbia.edu>
2020-10-19 07:11:42 +0000

    add view2 and view3

--- project/squirrel/urls.py
+++ project/squirrel/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name='squirrel'
+
 urlpatterns = [
         path('',views.index),
+        path('<int:squirrel_id>/',views.detail,name='detail'),
 ]
--- project/squirrel/views.py
+++ project/squirrel/views.py
@@ -1,4 +1,22 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+
+from .models import Squirrel
 
 def index(request):
-    return render(request,'squirrel/index.html',{})
+    squirrels = Squirrel.objects.all()
+    context = {
+            'squirrels':squirrels,
+    }
+
+    return render(request,'squirrel/index.html',context)
+
+def detail(request,squirrel_id):
+    squirrel =get_object_or_404(Squirrel, pk=squirrel_id)
+
+    context ={
+            'squirrel':squirrel,
+    }
+
+    return render(request,'squirrel/detail.html',context)
+
